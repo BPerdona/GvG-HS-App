2b3cd494c1f3be03a572480cb751af19a1c9cbe5
Bruno Perdona <bruno15perdona@gmail.com>
2022-06-27 23:53:53 -0300

    Fixing data class Card

--- app/src/main/java/br/com/gvg_hs_app/data/Card.kt
+++ app/src/main/java/br/com/gvg_hs_app/data/Card.kt
@@ -5,5 +5,11 @@ data class Card(
     val name: String,
     val type: String,
     val img: String,
-
+    val cost: Int,
+    val attack: Int = 0,
+    val health: Int = 0,
+    val artist: String,
+    val rarity: String,
+    val flavor: String,
+    val playerClass: String
 )
